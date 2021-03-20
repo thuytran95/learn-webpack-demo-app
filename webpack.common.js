a56f4c85c748877e0bf681f087b06d1d65266170
Tran Thuy <thuytt0295@gmail.com>
2021-03-20 21:59:22 +0700

    add prod and dev config

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,7 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
